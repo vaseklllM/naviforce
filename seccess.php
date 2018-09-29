021dc354e875b1d4b9b7ec5ecfc25845c2c07d2d
vasek <vasia.motnin@gmail.com>
2018-09-29 12:28:13 +0300

    work adaptive

---
+++ seccess.php
@@ -0,0 +1,27 @@
+<?php    
+session_start(); 
+    if($_GET["temppages"] == 1 ){        
+    }
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/seccess_style.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <title>Document</title>
+</head>
+<body>
+   <div class="content">
+       <div class="center-block">
+           <h1>Спасибо за <br>
+             заявку!</h1>
+             <span>
+                    Мы перезвоним вам в течении 2 часов.
+             </span>
+       </div>
+   </div>
+</body>
+</html>
